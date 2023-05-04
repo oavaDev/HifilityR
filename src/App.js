8a6f98cb147a5180fc0754f732f70a429835f5f7
oavaDev <omarvega598@gmail.com>
2023-05-03 22:47:13 -0500

    feat(dashboard): add dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+import styles from './App.module.css';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
+      <div className={styles.main}>
+        <Dashboard />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
-
+import React, { useState, useRef } from 'react';
+import styles from './Dashboard.module.css';
+import { Link } from 'react-router-dom';
+import {
+  AiOutlineHome,
+  AiFillHdd,
+  AiOutlineShoppingCart,
+  AiFillBook,
+} from 'react-icons/ai';
+import { BsBook } from 'react-icons/bs';
+import { BsHeadphones } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
 const Dashboard = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  const items = [
+    {
+      name: 'Home',
+      path: '/',
+      icon: <AiOutlineHome />,
+    },
+    {
+      name: 'Productos',
+      path: '/products',
+      icon: <BsHeadphones />,
+    },
+    {
+      name: 'Pedidos',
+      path: '/orders',
+      icon: <AiFillHdd />,
+    },
+    {
+      name: 'Carrito',
+      path: '/cart',
+      icon: <AiOutlineShoppingCart />,
+    },
+  ];
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div
+      style={{
+        transition: 'all .5s linear',
+      }}
+      className={styles.dashboard}
+    >
+      <div
+        style={{
+          width: `${open ? 'auto' : '3rem'}`,
+          transition: 'all .5s linear',
+        }}
+        ref={ref}
+        className={styles.items}
+      >
+        {!open ? (
+          <AiFillBook
+            size={25}
+            onClick={() => (open === false ? setOpen(true) : setOpen(false))}
+          />
+        ) : (
+          <BsBook
+            size={25}
+            onClick={() => (open === false ? setOpen(true) : setOpen(false))}
+          />
+        )}
+        {open ? (
+          <div>Username</div>
+        ) : (
+          <div>
+            <BiUser size={25} />
+          </div>
+        )}
+        {open
+          ? items.map((item, i) => {
+              return (
+                <Link
+                  style={{ display: 'flex', gap: '1rem' }}
+                  key={i}
+                  to={item.path}
+                >
+                  <div>{item.icon}</div>
+                  <div>{item.name}</div>
+                </Link>
+              );
+            })
+          : items.map((item) => {
+              return (
+                <Link key={item.name} to={item.path}>
+                  <div>{item.icon}</div>
+                </Link>
+              );
+            })}
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
