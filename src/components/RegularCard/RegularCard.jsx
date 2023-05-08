c59fcd6819c5539185007ccbcc0db9d9360e054d
oavaDev <omarvega598@gmail.com>
2023-05-07 20:33:26 -0500

    feat(home): add new card components to populate home

---
+++ src/components/RegularCard/RegularCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, Text, Col } from '@nextui-org/react';
+const RegularCard = ({ subtitle, title, img, footer, width, height }) => {
+  return (
+    <Card
+      css={{ w: `${width ? width : '100%'}`, h: `${height ? height : null}` }}
+    >
+      <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+        <Col>
+          <Text size={12} weight='bold' transform='uppercase' color='#ffffffAA'>
+            {subtitle}
+          </Text>
+          <Text h4 color='white'>
+            {title}
+          </Text>
+        </Col>
+      </Card.Header>
+      <Card.Image
+        src={img}
+        width='100%'
+        height='100%'
+        objectFit='cover'
+        alt='Card image background'
+      />
+      {footer && (
+        <Card.Footer
+          isBlurred
+          css={{
+            position: 'absolute',
+            bgBlur: '#0f111466',
+            borderTop: '$borderWeights$light solid $gray800',
+            bottom: 0,
+            zIndex: 1,
+          }}
+        >
+          <Text color='#c3c3c3'>{footer}</Text>
+        </Card.Footer>
+      )}
+    </Card>
+  );
+};
+
+export default RegularCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import styles from './Home.module.css';
+import RegularCard from '../../components/RegularCard/RegularCard';
 const Home = () => {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.home_news}>
+        <RegularCard
+          footer={'Nueva llegada, Kiwi Ears! Disfruta de los nuevos modelos'}
+          img={
+            'https://cdn.shopify.com/s/files/1/0040/7201/3924/files/home-cadenza-min.jpg?v=1681121730'
+          }
+        />
+        <RegularCard
+          subtitle={'Nuevos in-ears en camino'}
+          title={'Thieaudio Monarch'}
+          img={
+            'https://cdn.shopify.com/s/files/1/0040/7201/3924/files/home-monarch-min.jpg?v=1681121810'
+          }
+        />
+        <RegularCard
+          subtitle={'Lo nuevo de TRN '}
+          title={'TRN Kirin'}
+          img={
+            'https://cdn.shopify.com/s/files/1/0040/7201/3924/files/IMG_0684.jpg?v=1662538594'
+          }
+          height={'30%'}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
