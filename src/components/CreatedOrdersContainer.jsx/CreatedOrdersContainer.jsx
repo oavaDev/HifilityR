0f49b9493743e5968868a1927f473df0cddf4eb8
oavaDev <omarvega598@gmail.com>
2023-06-13 11:13:31 -0500

    feat(orderDetails): add more details to roder details

--- src/components/CreatedOrdersContainer.jsx/CreatedOrdersContainer.jsx
+++ src/components/CreatedOrdersContainer.jsx/CreatedOrdersContainer.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import styles from './CreatedOrdersContainer.module.css';
 import { useLocation } from 'react-router-dom';
-import { Card, Container, Grid, Row, Text,Col } from '@nextui-org/react';
+import { Card, Container, Grid, Text, Col } from '@nextui-org/react';
 const CreatedOrdersContainer = ({ token, user }) => {
   const [orders, setOrders] = useState();
   const location = useLocation();
   let { orderId } = location.state;
-
+  const [itemQuantity, setItemQuantity] = useState();
   const getOrders = () => {
     fetch(`http://localhost:8080/api/orders/${user.sub}/details/${orderId}`, {
       method: 'GET',
@@ -18,33 +18,38 @@ const CreatedOrdersContainer = ({ token, user }) => {
       .then((response) => response.json())
       .then((data) => {
         const createObject = (arrayOfArrays) => {
-            const arrayOfObjects = [];
-            if (arrayOfArrays === undefined) return console.log('undefined');
-            for (let i = 0; i < arrayOfArrays.length; i++) {
-              const array = arrayOfArrays[i];
-        
-              const newObj = {};
-        
-              newObj.clientName = array[0];
-              newObj.orderId = array[1];
-              newObj.date = array[2];
-              newObj.status = array[3];
-              newObj.productName = array[4];
-              newObj.quantity = array[5];
-        
-              arrayOfObjects.push(newObj);
-            }
-            return arrayOfObjects;
-          };
+          const arrayOfObjects = [];
+          if (arrayOfArrays === undefined) return console.log('undefined');
+          for (let i = 0; i < arrayOfArrays.length; i++) {
+            const array = arrayOfArrays[i];
+
+            const newObj = {};
+
+            newObj.clientName = array[0];
+            newObj.orderId = array[1];
+            newObj.date = array[2];
+            newObj.status = array[3];
+            newObj.productName = array[4];
+            newObj.quantity = array[5];
+
+            arrayOfObjects.push(newObj);
+          }
+          return arrayOfObjects;
+        };
+        const iQuantity = createObject(data[0]).reduce((acc, item) => {
+          return acc + item.quantity;
+        }, 0);
+        setItemQuantity(iQuantity); 
         setOrders(createObject(data[0]));
       });
   };
   useEffect(() => {
     user.sub && getOrders();
   }, []);
-  orders && console.log(orders);
-  return <div className={styles.Main}>
-    <Container>
+  console.log(orders)
+  return (
+    <div className={styles.Main}>
+      <Container>
         <Card>
           <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
             <Col>
@@ -63,7 +68,12 @@ const CreatedOrdersContainer = ({ token, user }) => {
               <Text h4 color='white'>
                 Información:
               </Text>
-             
+              <Text h5 color='white'>
+                Correo: {user && user.nombre}
+              </Text>
+              <Text h5 color='white'>
+                Cantidad de items: {itemQuantity && itemQuantity}
+              </Text>
             </Col>
           </Card.Header>
           <Card.Image
@@ -75,37 +85,35 @@ const CreatedOrdersContainer = ({ token, user }) => {
           />
         </Card>
       </Container>
-      <div className={styles.Prod} >
-      {
-        orders && orders.map((item, index) => {
+      <div className={styles.Prod}>
+        {orders &&
+          orders.map((item, index) => {
             return (
-                <Grid xs={6} sm={3} key={index}>
-                    <Card>
-                        <Card.Body css={{ p: 0 }}>
-                            <Card.Image
-                                src={
-                                    'https://res.cloudinary.com/dj80e8qqp/image/upload/v1668620919/657417-867137095_m1i39p.jpg'
-                                }
-                                objectFit='cover'
-                                width='100%'
-                                height={140}
-                                alt={item.title}
-                            />
-                        </Card.Body>
-                        <Card.Footer css={{ justifyItems: 'flex-start' }}>
-                            <Text h5 color='black'>
-                                {item.productName}
-                            </Text>
-                           
-                        </Card.Footer>
-                    </Card>
-                </Grid>
-            )  
-        })
-      }
+              <Grid xs={6} sm={3} key={index}>
+                <Card>
+                  <Card.Body css={{ p: 0 }}>
+                    <Card.Image
+                      src={
+                        'https://res.cloudinary.com/dj80e8qqp/image/upload/v1668620919/657417-867137095_m1i39p.jpg'
+                      }
+                      objectFit='cover'
+                      width='100%'
+                      height={140}
+                      alt={item.title}
+                    />
+                  </Card.Body>
+                  <Card.Footer css={{ justifyItems: 'flex-start' }}>
+                    <Text h5 color='black'>
+                      {item.productName}
+                    </Text>
+                  </Card.Footer>
+                </Card>
+              </Grid>
+            );
+          })}
       </div>
-     
-  </div>;
+    </div>
+  );
 };
 
 export default CreatedOrdersContainer;
